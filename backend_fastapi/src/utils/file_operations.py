f5ab9cf6c6e614ee01db6ed0cc43d9b7e49207ba
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-07-24 22:07:30 +0530

    added recursive character text splitter, also ensure rate limiting, make it better by splitting into 1/10th parts

--- backend_fastapi/src/utils/file_operations.py
+++ backend_fastapi/src/utils/file_operations.py
@@ -1,5 +1,6 @@
 import os
 import chardet
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def detect_encoding(file_path):
     """Detect the encoding of a file."""
@@ -8,15 +9,18 @@ def detect_encoding(file_path):
     result = chardet.detect(raw_data)
     return result['encoding']
 
-def split_text_into_chunks(file_path, encoding,chunk_size=500):
+def split_text_into_chunks(file_path, encoding,chunk_size=2000,chunk_overlap = 200):
     with open(file_path, 'r', encoding=encoding,errors = 'ignore') as file:
         text = file.read()
 
-    # Tokenize the text (split into words)
-    tokens = text.split()
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        separators=["\n\n", "\n", ".", " ", ""]
+    )
 
-    # Split tokens into chunks of 1000 tokens each
-    chunks = [' '.join(tokens[i:i + chunk_size]) for i in range(0, len(tokens), chunk_size)]
+    # Split the text into chunks
+    chunks = splitter.split_text(text)
 
     return chunks
 
